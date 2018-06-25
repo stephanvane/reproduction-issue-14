dc4f7031ae1db07f191908b852b930189aab902d
Stephan van Eijkelenburg <stephanvane@gmail.com>
2018-06-25 12:24:28 +0200

    .

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <nuxt-link to="page1">Go to page 1</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ pages/page1.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div>
+      <nuxt-link to="/">Go to the homepage</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
